4f8cb51d0195b4e00eceb2a5c122aacd917af6d1
vaakavak <32431682+vaakavak@users.noreply.github.com>
2024-02-19 20:48:39 +0400

    update func

--- Func/Func/Program.cs
+++ Func/Func/Program.cs
@@ -1,15 +1,30 @@
 ﻿
 
-Console.WriteLine("Привет");
-Console.WriteLine("Ты здесь?");
-WriteError("Нет соединения с интернетом.");
-Console.WriteLine("Странно...");
-WriteError("Интернет не оплачен");
+//Console.WriteLine("Привет");
+//Console.WriteLine("Ты здесь?");
+//WriteError("Нет соединения с интернетом.");
+//Console.WriteLine("Странно...");
+//WriteError("Интернет не оплачен");
 
-static void WriteError(string text)
+//static void WriteError(string text)
+//{
+//    ConsoleColor defaultColor = Console.ForegroundColor;
+//    Console.ForegroundColor = ConsoleColor.Red;
+//    Console.WriteLine(text);
+//    Console.ForegroundColor = defaultColor;
+//}
+
+
+
+
+
+int x = 7, y = 9, sum;
+sum = Sum(x, y) + x + 12;
+Console.WriteLine(sum);
+
+static int Sum(int x, int y)
 {
-    ConsoleColor defaultColor = Console.ForegroundColor;
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine(text);
-    Console.ForegroundColor = defaultColor;
+    int sum;
+    sum = x + y;
+    return sum;
 }
