095de819f8df7bc32dfbb8b5f8ecd2db727cb72f
vaakavak <32431682+vaakavak@users.noreply.github.com>
2024-02-19 16:01:43 +0400

    created new project Func

---
+++ Func/Func/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+Console.WriteLine("Привет");
+Console.WriteLine("Ты здесь?");
+WriteError();
+Console.WriteLine("Странно...");
+WriteError();
+
+static void WriteError()
+{
+    ConsoleColor defaultColor = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Нет соединения с интернетом.");
+    Console.ForegroundColor = defaultColor;
+}
