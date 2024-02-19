a14324fb4bcd667441b2316cf0cc5cb3680842b2
vaakavak <32431682+vaakavak@users.noreply.github.com>
2024-02-19 14:58:50 +0400

    create foreach

---
+++ CycleForEarch/CycleForEarch/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вывод массива по индексу
+
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+
+
+// Вывод элементов массива 
+
+foreach (int number in numbers)
+{
+    Console.Write(number + " ");
+}  
