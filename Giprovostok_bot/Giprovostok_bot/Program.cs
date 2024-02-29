8d2047a9f314b1eab9ede2bc897949e0a0b07e35
vaakavak <32431682+vaakavak@users.noreply.github.com>
2024-02-29 15:22:23 +0400

    created bot telegram

---
+++ Giprovostok_bot/Giprovostok_bot/Program.cs
@@ -0,0 +1,25 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+
+internal class Program
+{
+    private static TelegramBotClient _botClient;
+    private static void Main(string[] args)
+    {
+        _botClient = new TelegramBotClient("7142419618:AAEvA - MOJsRzh2tYQ2QeD1cCxl4E--9G7LM");
+        _botClient.OnMessage += Bot_OnMessage;
+        _botClient.StartReceiving();
+
+        Console.WriteLine("Bot started. Press any key to exit...");
+
+        // Отправляем приветственное сообщение при запуске бота
+        _botClient.SendTextMessageAsync("YOUR_CHAT_ID", "Привет!");
+
+        Console.ReadKey();
+
+        _botClient.StopReceiving();
+        
+    }
+
+}
