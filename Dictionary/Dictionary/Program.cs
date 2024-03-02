bd2184400e88365e95e7b23546f5a420ccb012b8
vaakavak <32431682+vaakavak@users.noreply.github.com>
2024-03-02 19:22:31 +0400

    add dictionary

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, string> counrtiesCapitals = new Dictionary<string, string>();
+            counrtiesCapitals.Add("Австралия", "Конберра");
+            counrtiesCapitals.Add("Турция", "Анкара");
+            counrtiesCapitals.Add("Швейцария", "Берн");
+
+            //counrtiesCapitals.Remove("Турция"); // удаление элементов
+
+            //if (counrtiesCapitals.ContainsKey("Австралия"))
+            //    Console.WriteLine(counrtiesCapitals["Австралия"]);  // вывод значения по ключу
+
+            foreach (var item in counrtiesCapitals)
+            {
+                Console.WriteLine($"Страна {item.Key} - столица {item.Value}");
+            }
+
+            foreach (var key in counrtiesCapitals.Keys)
+            {
+                Console.WriteLine($"Страна - {key}") ;
+            }
+
+            foreach (var value in counrtiesCapitals.Values)
+            {
+                Console.WriteLine($"Столица - {value}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
