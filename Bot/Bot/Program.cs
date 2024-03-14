8333f5a86107621357144d584ef72101ee38d0d7
vaakavak <32431682+vaakavak@users.noreply.github.com>
2024-03-14 14:03:11 +0400

    fix bot

--- Bot/Bot/Program.cs
+++ Bot/Bot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Polling;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramBotExperiments
 {
@@ -8,6 +9,7 @@ namespace TelegramBotExperiments
     class Program
     {
         static ITelegramBotClient bot = new TelegramBotClient("7142419618:AAEvA-MOJsRzh2tYQ2QeD1cCxl4E--9G7LM");
+
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
             // Некоторые действия
@@ -17,7 +19,15 @@ namespace TelegramBotExperiments
                 var message = update.Message; // Создаем новую переменную для удобства и записываем в нее всю информацию о пришедшем сообщении.
                 if (message.Text.ToLower() == "/start") // Проверяем какой текст отправил пользователь. Если текст сообщения в нижнем регистре (.ToLower()) является словом "/start", то пишем ему сообщение "......". И останавливаем выполнение метода командой return.
                 {
-                    await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать в бот помошник Гипровостока. Введите команду /help для получения помощи. ");
+                    await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать в бот помошник Гипровостока. Выберите нужную вам опцию:", replyMarkup: new InlineKeyboardMarkup(new[]
+                    {
+new[]
+{
+InlineKeyboardButton.WithCallbackData("Оставить заявку", "option1"),
+InlineKeyboardButton.WithCallbackData("Справка", "option2"),
+InlineKeyboardButton.WithCallbackData("О компании", "option3")
+}
+}));
                     return;
                 }
                 else if (message.Text.ToLower() == "/help")
@@ -25,9 +35,27 @@ namespace TelegramBotExperiments
                     await botClient.SendTextMessageAsync(message.Chat, "Бот сздан для помощи в решении проблем в Гипровостоке. Вы можете оставить заявку, обязательно укажите ФИО, Отдел и Вашу поблему");
                     return;
                 }
-                await botClient.SendTextMessageAsync(message.Chat, "Вы отправили текстовое сообщение без команды. Я пока не умею работать с этим сообщением. Попробуйте позже...");
+                await botClient.SendTextMessageAsync(message.Chat, "Вы отправили текстовое сообщение без команды. Я пока не умею работать с этим сообщением. Попробуйте позже...");
             }
         }
+
+        public static async Task HandleCallbackQueryAsync(ITelegramBotClient botClient, CallbackQuery callbackQuery, CancellationToken cancellationToken)
+        {
+            // Обработка нажатия кнопок
+            switch (callbackQuery.Data)
+            {
+                case "option1":
+                    await botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id, "**Введите ваше ФИО | Ваш отдел | Проблему, которая у вас возникла:**");
+                    break;
+                case "option2":
+                    await botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id, "Вы выбрали опцию \"Справка\"");
+                    break;
+                case "option3":
+                    await botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id, "Вы выбрали опцию \"О компании\"");
+                    break;
+            }
+        }
+
 
         public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
@@ -47,10 +75,10 @@ namespace TelegramBotExperiments
                 AllowedUpdates = { }, // получать все типы обновлений
             };
             bot.StartReceiving(
-                HandleUpdateAsync,
-                HandleErrorAsync,
-                receiverOptions,
-                cancellationToken
+            HandleUpdateAsync,
+            HandleErrorAsync,
+            receiverOptions,
+            cancellationToken
             );
             Console.ReadLine();
         }
---
+++ Words/Words/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Words
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(5, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(906, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Добро пожаловать в программу изучения иностранных языков";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 57);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(781, 79);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "MemoryBoost";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(67, 142);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(376, 102);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Карточки";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(474, 142);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(376, 102);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Тренировочные карточки";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(67, 250);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(376, 102);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Собери слово";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(474, 250);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(376, 102);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Выбери перевод";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(67, 358);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(376, 102);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Верно-Неверно";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(474, 358);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(376, 102);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Мемория";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(916, 550);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
